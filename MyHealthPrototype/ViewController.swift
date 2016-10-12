22a819020180f108742b4df1a4592fbe269a632c
guillianbalisi <guillianbalisi@gmail.com>
2016-10-12 13:14:14 -0400

    Initial Commit

---
+++ MyHealthPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHealthPrototype
+//
+//  Created by Guillian Balisi on 2016-10-12.
+//  Copyright © 2016 Guillian Balisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
